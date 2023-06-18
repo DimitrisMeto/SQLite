a455fad004557c85e946936a52f0ab676527b326
DimitrisMeto <123649144+DimitrisMeto@users.noreply.github.com>
2023-06-18 18:15:42 +0300

    Update main.py
    creating a database to store books(finished project)

--- main.py
+++ main.py
@@ -48,5 +48,14 @@ def edit():
     return render_template("edit.html", book=book_selected)
 
 
+@app.route("/delete")
+def delete():
+    book_id = request.args.get("id")
+    book_to_delete = db.session.execute(db.select(Books).where(Books.id == book_id)).scalar()
+    db.session.delete(book_to_delete)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
 if __name__ == "__main__":
     app.run(debug=True)
