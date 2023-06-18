a00ad2cd9bbfa15e004b723c7a03ea2e76ba7a59
DimitrisMeto <123649144+DimitrisMeto@users.noreply.github.com>
2023-06-18 17:58:52 +0300

    Add files via upload
    Creating a database to store books you read (using Flask, SQLite and SQLAlchemy)

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///books-collection.db"
+db.init_app(app)
+
+
+class Books(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+@app.route('/')
+def home():
+    all_books = db.session.execute(db.select(Books)).scalars()
+    return render_template("index.html", books=all_books)
+
+
+@app.route("/add", methods=["POST", "GET"])
+def add():
+    if request.method == "POST":
+        new_book = Books(title=request.form["book"], author=request.form["author"], rating=request.form["rating"])
+        with app.app_context():
+            db.session.add(new_book)
+            db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add.html")
+
+
+@app.route("/edit", methods=["POST", "GET"])
+def edit():
+    if request.method == "POST":
+        book_id = request.form["id"]
+        book_to_update = db.session.execute(db.select(Books).where(Books.id == book_id)).scalar()
+        book_to_update.rating = request.form["rating"]
+        db.session.commit()
+        return redirect(url_for("home"))
+    book_id = request.args.get("id")
+    book_selected = db.session.execute(db.select(Books).where(Books.id == book_id)).scalar()
+    return render_template("edit.html", book=book_selected)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
